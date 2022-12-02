cc0f9f7e24b135c88786e8482066b20f36ad4d7b
Lasse Lejon <lasse.lejon@hotmail.com>
2022-12-02 15:39:50 +0100

    Changed to use dictionary.

--- day-02/sol.py
+++ day-02/sol.py
@@ -5,21 +5,9 @@
 
 # X=lose, Y=draw, Z=win
 
-def getRoundOutcomeScore(outcome):
-    score = 0
-    if outcome == 'X':
-        score = 0
-    if outcome == 'Y':
-        score = 3
-    if outcome == 'Z':
-        score = 6
-    return score
-
-
 def getMyMove(outcome, opp):
     moveToWinningMove = {'A': 'B', 'C': 'A', 'B': 'C'}
     moveToLoosingMove = {'B': 'A', 'A': 'C', 'C': 'B'}
-    myMove = ""
     if outcome == 'Z':
         return moveToWinningMove[opp]
     if outcome == 'X':
@@ -31,9 +19,10 @@ def getMyMove(outcome, opp):
 lines = open("input.txt").readlines()
 totalScore = 0
 moveToScore = {'A': 1, 'B': 2, 'C': 3}
+roundOutcomeToScore = {'X': 0, 'Y': 3, 'Z': 6}
 for line in lines:
     opponentMove = line[:1]
     roundOutcome = line[2:3]
-    totalScore += getRoundOutcomeScore(roundOutcome)
+    totalScore += roundOutcomeToScore[roundOutcome]
     totalScore += moveToScore[getMyMove(roundOutcome, opponentMove)]
 print(totalScore)
