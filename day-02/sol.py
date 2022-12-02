5771375a823c948cd81499417b32fb94be9131c7
Lasse Lejon <lasse.lejon@hotmail.com>
2022-12-02 15:36:38 +0100

    Part 2 day 1 done.

--- day-02/sol.py
+++ day-02/sol.py
@@ -5,21 +5,35 @@
 
 # X=lose, Y=draw, Z=win
 
+def getRoundOutcomeScore(outcome):
+    score = 0
+    if outcome == 'X':
+        score = 0
+    if outcome == 'Y':
+        score = 3
+    if outcome == 'Z':
+        score = 6
+    return score
+
+
+def getMyMove(outcome, opp):
+    moveToWinningMove = {'A': 'B', 'C': 'A', 'B': 'C'}
+    moveToLoosingMove = {'B': 'A', 'A': 'C', 'C': 'B'}
+    myMove = ""
+    if outcome == 'Z':
+        return moveToWinningMove[opp]
+    if outcome == 'X':
+        return moveToLoosingMove[opp]
+    else:
+        return opp
+
+
 lines = open("input.txt").readlines()
 totalScore = 0
-move_winning_condition = {"X": "C", "Y": "A", "Z": "B"}
-mymove_to_opponent = {"X": "A", "Y": "B", "Z": "C"}
+moveToScore = {'A': 1, 'B': 2, 'C': 3}
 for line in lines:
     opponentMove = line[:1]
-    myMove = line[2:3]
-    if myMove == 'X':
-        totalScore += 1
-    if myMove == 'Y':
-        totalScore += 2
-    if myMove == 'Z':
-        totalScore += 3
-    if move_winning_condition[myMove] == opponentMove:
-        totalScore += 6
-    if mymove_to_opponent[myMove] == opponentMove:
-        totalScore += 3
+    roundOutcome = line[2:3]
+    totalScore += getRoundOutcomeScore(roundOutcome)
+    totalScore += moveToScore[getMyMove(roundOutcome, opponentMove)]
 print(totalScore)
