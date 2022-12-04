0dbfc56f73970d9dd3df31f07c701d1c015e8f26
Lasse Lejon <lasse.lejon@hotmail.com>
2022-12-04 11:31:14 +0100

    Day 4 part 2 done.

--- day-04/sol.py
+++ day-04/sol.py
@@ -3,9 +3,23 @@ def p1(lines):
     for line in lines:
         sp = line.split(',')
         a, b = sp[0].split('-'), sp[1].split('-')
-        if int(a[0]) <= int(b[0]) and int(a[1]) >= int(b[1]) or int(b[0]) <= int(a[0]) and int(b[1]) >= int(a[1]):
+        a[0], a[1], b[0], b[1] = int(a[0]), int(a[1]), int(b[0]), int(b[1])
+        if a[0] <= b[0] and a[1] >= b[1] or b[0] <= a[0] and b[1] >= a[1]:
             ans += 1
     print(ans)
 
 
-p1(open("input.txt").read().split('\n'))
+def p2(lines):
+    ans = 0
+    for line in lines:
+        sp = line.split(',')
+        a, b = sp[0].split('-'), sp[1].split('-')
+        r1, r2 = range(int(a[0]), int(a[1])+1), range(int(b[0]), int(b[1])+1)
+        for i in r1:
+            if i in r2:
+                ans += 1
+                break
+    print(ans)
+
+
+p2(open("input.txt").read().split('\n'))
